b2ca2a5bbb5ecdeba3e2841e2743996353694433
Fyuk11 <rodrigomez.11e@gmail.com>
2025-08-31 10:52:42 -0300

    Actualización de estilos

--- js/scroll.js
+++ js/scroll.js
@@ -22,3 +22,14 @@ function initScroll() {
     });
   });
 };
+
+
+// scroll.js
+document.addEventListener("scroll", () => {
+  const header = document.querySelector(".header");
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
